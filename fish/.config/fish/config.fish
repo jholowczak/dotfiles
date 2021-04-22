c25cf19bd21cda16966c5ef964535ab019ca9bde
John Holowczak <jholowczak@gmail.com>
2021-04-22 12:29:40 -0400

    Fish updates for emacs vterm

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -31,6 +31,18 @@ if test -n "$SSH_CONNECTION"
     end
 end
 
+function vterm_printf;
+    if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end 
+        # tell tmux to pass the escape sequences through
+        printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+    else if string match -q -- "screen*" "$TERM"
+        # GNU screen (screen, screen-256color, screen-256color-bce)
+        printf "\eP\e]%s\007\e\\" "$argv"
+    else
+        printf "\e]%s\e\\" "$argv"
+    end
+end
+
 if test -n "$DESKTOP_SESSION"
     set (gnome-keyring-daemon --start | string split "=")
 end
