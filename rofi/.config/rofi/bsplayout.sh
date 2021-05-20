8c1bc5f3c9f85df7622086c18ca5b32a87adca64
John Holowczak <jholowczak@gmail.com>
2021-05-20 10:02:53 -0400

    Updates

--- rofi/.config/rofi/bsplayout.sh
+++ rofi/.config/rofi/bsplayout.sh
@@ -5,15 +5,16 @@ if [ $# -eq 0 ]; then
     choices+=( "clear" )
     printf '%s\n' "${choices[@]}"
 else
+    $desktop=$(bspc query -D -d focused --names)
     case $1 in
         clear)
             bsp-layout remove
         ;;
         tall|rtall)
-            bsp-layout set $1 -- --master-size 0.7
+            bsp-layout set $1 $desktop --master-size 0.7
         ;;
         *)
-            bsp-layout set $1 --
+            bsp-layout set $1 $desktop
         ;;
     esac
     killall rofi
