ed67b4115ea5863f7f420f01bf18c00da48d5180
John Holowczak <jholowczak@gmail.com>
2022-07-22 09:40:24 -0400

    move miniconda to home directory

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -62,6 +62,6 @@ if [ -f '/home/skip/google-cloud-sdk/path.fish.inc' ]; . '/home/skip/google-clou
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /opt/miniconda3/bin/conda "shell.fish" "hook" $argv | source
+eval ~/miniconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
