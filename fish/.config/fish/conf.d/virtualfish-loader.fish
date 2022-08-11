9a05207ddee57cd6af9bb56b88507cb40ac2e126
John Holowczak <jholowczak@gmail.com>
2022-08-10 22:50:39 -0400

    Add markdown to nvim

--- fish/.config/fish/conf.d/virtualfish-loader.fish
+++
@@ -1,3 +0,0 @@
-set -g VIRTUALFISH_VERSION 2.5.3
-source /home/john/.local/lib/python3.9/site-packages/virtualfish/virtual.fish
-emit virtualfish_did_setup_plugins
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -36,6 +36,14 @@ require('packer').startup(function(use)
     end
   }
   use { 'ctrlpvim/ctrlp.vim' }
+  use({
+    "iamcco/markdown-preview.nvim",
+    run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
+
   use {
     'Shatur/neovim-session-manager',
     requires = { 'nvim-lua/plenary.nvim' },
