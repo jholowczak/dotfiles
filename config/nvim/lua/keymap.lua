fc0ae9dea160e8a4c603c64112f7c4d2c94db8da
John Holowczak <jholowczak@gmail.com>
2023-11-20 15:20:54 -0500

    migrate FileType bindings from old vim config, add go plugin

--- config/nvim/lua/keymap.lua
+++ config/nvim/lua/keymap.lua
@@ -28,6 +28,8 @@ Kmap("n", "<l>vl", ":lua tmux_send_last_command()<cr>")
 --Kmap("n", "<l>mb", ":lua tmux_send_command('git blame -L ' .. vim.fn.line('.') .. ',' .. vim.fn.line('.') .. ' ' .. vim.fn.expand('%:p'))<cr>", opts)
 Kmap('n', '<l>dd', '<cmd>lua vim.diagnostic.open_float()<cr>')
 Kmap("n", "<l>co", ":edit ~/.config/nvim/init.lua<CR>")
+Kmap("n", "<l>ck", ":edit ~/.config/nvim/lua/keymap.lua<cr>")
+Kmap("n", "<l>cp", ":edit ~/.config/nvim/lua/plugins.lua<cr>")
 Kmap("n", "<l>cl", ":source ~/.config/nvim/init.lua<cr>")
 Kmap("n", "<l>ms", "<cmd>lua require('neogit').open({ kind = \"split\" })<cr>")
 vim.keymap.set({ "n", "x" }, "<leader>rs", function() require("ssr").open() end)
@@ -48,3 +50,92 @@ Kmap('n', "<C-k>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateUp()<cr>"
 Kmap('n', "<C-l>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateRight()<cr>")
 Kmap('n', "<C-\\>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateLastActive()<cr>")
 --Kmap('n', "<C-Space>", ":lua require'nvim-tmux-navigation'.NvimTmuxNavigateNext()<cr>", opts)
+--
+local fileTypeBindings = {
+    {   pattern = { "c", "cpp", "ocaml" },
+        binds = {
+            { keys = "<leader>gr",
+              cmd  = ":!make run<cr>"},
+            { keys = "<leader>gt",
+              cmd  = ":!make test<cr>"},
+            { keys = "<leader>gb",
+              cmd  = ":!make<cr>"}
+        }
+    },
+    {   pattern = { "c", "cpp", "javascript" },
+        binds = {
+            { keys = "<leader>gf",
+              cmd  = ":!ClangFormat<cr>" }
+        }
+    },
+    {   pattern = { "haskell" },
+        binds = {
+            { keys = "<leader>gr",
+              cmd  = ":!stack run<cr>" },
+            { keys = "<leader>gt",
+              cmd  = ":!stack test<cr>" },
+            { keys = "<leader>gb",
+              cmd  = ":!stack build<cr>" },
+            { keys = "<leader>ge",
+              cmd  = ":!stack %" },
+            { keys = "<leader>t",
+              cmd  = ":!GhcModType<cr>" }
+        }
+    },
+    {   pattern = { "rust" },
+        binds = {
+            { keys = "<leader>gr",
+              cmd  = ":Cargo run<cr>" },
+            { keys = "<leader>gt",
+              cmd  = ":Cargo test<cr>" },
+            { keys = "<leader>gc",
+              cmd  = ":Cargo check<cr>" },
+            { keys = "<leader>gb",
+              cmd  = ":Cargo build<cr>" },
+            { keys = "<leader>gB",
+              cmd  = ":Cargo build --release<cr>" }
+        }
+    },
+    {   pattern = { "go" },
+        binds = {
+            { keys = "<leader>gr",
+              cmd  = ":GoRun<cr>" },
+            { keys = "<leader>gt",
+              cmd  = ":GoTest<cr>" },
+            { keys = "<leader>gb",
+              cmd  = ":GoBuild<cr>" },
+            { keys = "<leader>gf",
+              cmd  = ":!cd %:p:h && go test && cd -<cr>" },
+            { keys = "<leader>t",
+              cmd  = ":GoInfo<cr>" },
+        }
+    }
+}
+
+for _, lang in pairs(fileTypeBindings) do
+    for _, block in pairs(lang.binds) do
+        vim.api.nvim_create_autocmd("FileType", {
+            pattern = lang.pattern,
+            callback = function()
+                Kmap('n', block.keys, block.cmd, {noremap = true})
+            end
+        })
+    end
+end
+
+--if has('macunix')
+--    autocmd FileType python nnoremap <buffer><leader>rr :!python3 %<cr>
+--    autocmd FileType python nnoremap <buffer><leader>rt :!python3 -m pytest -s %<cr>
+--else
+--    autocmd FileType python nnoremap <buffer><leader>rr :!python %<cr>
+--    autocmd FileType python nnoremap <buffer><leader>rt :!python -m pytest -s %<cr>
+--endif
+--autocmd FileType python nnoremap <buffer><leader>rl :!pylint %<cr>
+--autocmd FileType python nnoremap <buffer><leader>rb :!/usr/bin/black %<cr>
+--autocmd FileType rust let g:rustfmt_autosave = 1
+--
+--" vimux-golang
+--autocmd FileType go nnoremap <buffer><leader>rt :wa<CR> :GolangTestCurrentPackage<CR>
+--autocmd FileType go nnoremap <buffer><leader>rf :wa<CR> :GolangTestFocused<CR>
+--
+--autocmd FileType sh nnoremap <buffer><leader>rr :!./%<CR>
