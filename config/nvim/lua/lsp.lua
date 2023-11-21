815b502c221aec7d431621387f79b7a3d1921bf1
John Holowczak <jholowczak@gmail.com>
2023-11-20 23:34:03 -0500

    fix lsp/navic attachment for rust

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -57,14 +57,14 @@ end
 local rt = require('rust-tools')
 rt.setup({
   server = {
-    on_attach = function(_, bufnr)
+    on_attach = function(client, bufnr)
       -- Hover actions
       vim.keymap.set("n", "<S-tab>", rt.hover_actions.hover_actions, { buffer = bufnr })
       -- Code action groups
       vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-      --my_on_attach(_, bufnr)
+      my_on_attach(client, bufnr)
     end,
   },
 })
 rt.inlay_hints.enable()
-vim.g.coq_settings = { limits = {completion_auto_timeout = 0.2 } } 
+vim.g.coq_settings = { limits = {completion_auto_timeout = 0.2 } }
